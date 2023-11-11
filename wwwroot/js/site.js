39fb497e169e521dbde7651efe44e17b633d4239
CauaVini791 <cauaviniciussilvabarbosa@gmail.com>
2023-11-11 16:17:38 -0300

    Initial commit

---
+++ wwwroot/js/site.js
@@ -0,0 +1,35 @@
+﻿$(document).ready(
+    function() {
+        $("#formCadastro").submit(
+            function(e) {
+                e.preventDefault();
+                Cadastrar();
+            }
+        );
+    }
+);
+
+function Cadastrar() {
+    var parametros = {
+        Nome: $("#idNome").val(),
+        Email: $("#idEmail").val(),
+        Mensagem: $("#idMensagem").val()
+    };
+
+    $.post("/Home/Cadastrar", parametros)
+
+    .done(function(data) {
+        if(data.status == "OK") {
+            $("#formCadastro").after("<h3>Cadastro realizado com sucesso!</h3>");
+            $("#formCadastro").hide();
+        }
+        else {
+            $("#formCadastro").after("<h3>Erro. Tente novamente mais tarde!</h3>");
+            $("#formCadastro").hide();
+        }
+    })
+
+    .fail(function() {
+        alert("Erro");
+    })
+}
